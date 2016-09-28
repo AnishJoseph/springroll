7f8d54f63ab3dbe82045c74cb716bd37df557820
Anish Joseph <joseph.anish@gmail.com>
2016-09-28 11:15:19 +0530

    donnt fire job completion event if event is instance of ISignallingEvent

--- src/springroll-routers/src/main/java/com/springroll/router/JMSEntryPoint.java
+++ src/springroll-routers/src/main/java/com/springroll/router/JMSEntryPoint.java
@@ -2,6 +2,7 @@ package com.springroll.router;
 
 import com.springroll.core.IEvent;
 import com.springroll.core.ContextStore;
+import com.springroll.core.ISignallingEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
@@ -18,7 +19,8 @@ public class JmsEntryPoint {
     public void on(IEvent event) {
         /* Called when the event comes from JMS - it will be in a new thread so set these attributes for the new thread */
         ContextStore.put(event.getUser(), event.getJobId(), event.getLegId());
-        publisher.publishEvent(event);
+        if(!(event instanceof ISignallingEvent))
+            publisher.publishEvent(event);
 
     }
 
