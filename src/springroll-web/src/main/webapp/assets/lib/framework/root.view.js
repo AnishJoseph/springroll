33cac81beb7db73015f17a6e3d05a9e801e4d2ff
Anish Joseph <joseph.anish@gmail.com>
2016-10-17 07:30:54 +0530

    Added Indicator module that shows a message on the top of the screen

--- src/springroll-web/src/main/webapp/assets/lib/framework/root.view.js
+++ src/springroll-web/src/main/webapp/assets/lib/framework/root.view.js
@@ -2,15 +2,17 @@ define(['Application', 'marionette'], function (Application, Marionette) {
 
     Application.RootView = Marionette.View.extend({
         tagName: 'div',
-        template: _.template("<div id='main-menu'/><div id='main-body'/><div id='alerts'/>"),
+        template: _.template("<div id='main-menu'/><div id='main-body'/><div id='alerts'/><div id='indicator' style='position: absolute; top:0px; align-content: center; width: 100%'/> "),
         regions: {
             menuRegion: '#main-menu',
             bodyRegion: '#main-body',
-            alertsRegion: '#alerts'
+            alertsRegion: '#alerts',
+            indicatorRegion: '#indicator'
         },
         onRender: function() {
             this.showChildView('menuRegion', new Application.MenuView());
             this.showChildView('alertsRegion', Application.Alerts.getAlertPanelView());
+            this.showChildView('indicatorRegion', Application.Indicator.getView());
         },
         showBody : function(view){
             this.showChildView('bodyRegion', view);
