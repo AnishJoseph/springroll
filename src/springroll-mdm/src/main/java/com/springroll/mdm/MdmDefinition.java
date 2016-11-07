8156329c73d1ae8637eb3168858166989d16283e
Anish Joseph <joseph.anish@gmail.com>
2016-11-07 14:58:48 +0530

    1st rev of MDM insertion

--- src/springroll-mdm/src/main/java/com/springroll/mdm/MdmDefinition.java
+++ src/springroll-mdm/src/main/java/com/springroll/mdm/MdmDefinition.java
@@ -1,14 +1,21 @@
 package com.springroll.mdm;
 
+import com.springroll.core.exceptions.FixableException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 
 /**
  * Created by anishjoseph on 04/11/16.
  */
 public class MdmDefinition {
+    private static final Logger logger = LoggerFactory.getLogger(MdmDefinition.class);
     private String master;
+    private String masterClassName;
     private String dataQuery;
     private List<ColDef> colDefs;
+    private Class masterClass;
 
     public String getMaster() {
         return master;
@@ -32,6 +39,24 @@ public class MdmDefinition {
 
     public void setColDefs(List<ColDef> colDefs) {
         this.colDefs = colDefs;
+    }
+
+    public String getMasterClassName() {
+        return masterClassName;
+    }
+
+    public Class getMasterClass() {
+        return masterClass;
+    }
+
+    public void setMasterClassName(String masterClassName) {
+        this.masterClassName = masterClassName;
+        try {
+            masterClass = Class.forName(masterClassName);
+        } catch (ClassNotFoundException e) {
+            logger.error("Exception while getting finding MDM class with name   '{}'. Exceptions is {} ", masterClassName, e.getMessage());
+            throw new FixableException("", "lov.source.missingenum", masterClassName, e.getMessage());
+        }
     }
 
     public ColDef getColDefByName(String colName){
