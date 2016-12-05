9b3e99ed6f2c46d28350487624b5b2a252922639
Anish Joseph <joseph.anish@gmail.com>
2016-12-05 10:24:53 +0530

    Named queries can also exist in grid.json - helps faster development

---
+++ src/springroll-reporting/src/main/java/com/springroll/reporting/grid/NamedQuery.java
@@ -0,0 +1,25 @@
+package com.springroll.reporting.grid;
+
+/**
+ * Created by anishjoseph on 05/12/16.
+ */
+public class NamedQuery {
+    private String name;
+    private String query;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+}
