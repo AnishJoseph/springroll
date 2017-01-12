0c875edec49c3f58faf55b96d34fefde00346b2d
Anish Joseph <joseph.anish@gmail.com>
2017-01-12 16:20:39 +0530

    Fixed bug - now can handle . in menu item names

--- src/springroll-web/src/main/webapp/assets/lib/framework/menu.js
+++ src/springroll-web/src/main/webapp/assets/lib/framework/menu.js
@@ -19,13 +19,13 @@ Application.MenuView = Marionette.View.extend({
                 template.push('<a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">' + Localize(item.items[0].parent) + '<span class="caret"></span></a> ');
                 template.push('<ul class="dropdown-menu">');
                 _.each(_.sortBy(item.items, 'subIndex'), function (menuItem) {
-                    var id = 'menuId' + menuItem.name;
+                    var id = 'menuId' + menuItem.name.replace(/\./g,'');
                     template.push('<li role="presentation"><a id="' + id + '">' + Localize(menuItem.name) + '</a></li>');
                 });
                 template.push(' </ul> </li>');
             } else {
                 var menuItem = item.items;
-                var id = 'menuId' + menuItem.name;
+                var id = 'menuId' + menuItem.name.replace(/\./g,'');
                 template.push('<li role="presentation"><a id="' + id + '">' + Localize(menuItem.name) + '</a></li>');
             }
         });
@@ -56,14 +56,14 @@ Application.MenuView = Marionette.View.extend({
         _.each(Application.getMenuItems(), function(item){
             if($.isArray(item.items)){
                 _.forEach(item.items, function(menuItem){
-                    var id = 'click #menuId' + menuItem.name;
+                    var id = 'click #menuId' + menuItem.name.replace(/\./g,'');
                     events[id] = function () {
                         menuItem.controller.activate(menuItem.name);
                     }
                 });
             } else {
                 var menuItem = item.items;
-                var id = 'click #menuId' + menuItem.name;
+                var id = 'click #menuId' + menuItem.name.replace(/\./g,'');
                 events[id] = function () {
                     menuItem.controller.activate(menuItem.name);
                 }
