353c3e730fcad6de043b64048378ec54105a5408
Anish Joseph <joseph.anish@gmail.com>
2017-01-17 11:27:55 +0530

    Some optimization

--- src/springroll-web/src/main/webapp/assets/lib/modules/jobdashboard.js
+++ src/springroll-web/src/main/webapp/assets/lib/modules/jobdashboard.js
@@ -2,6 +2,7 @@ var Marionette =  require('backbone.marionette');
 var Application = require('Application');
 
 var dashboardView;
+var indexOfReviewLog = undefined;
 var JobDashboardController = Marionette.Object.extend({
     activate: function() {
         dashboardView = new Application.GridView({"gridName":"JobDashboard", "datamassager" : this.dataMassager });
@@ -9,15 +10,20 @@ var JobDashboardController = Marionette.Object.extend({
         Backbone.history.navigate('jobDashboard');
     },
     dataMassager : function(gridReport){
-        _.each(gridReport.data, function(row){
-            var reviewLogs = row[7];
-            var reviewStr = [];
-            if(reviewLogs != undefined){
-                _.each(reviewLogs, function(reviewLog){
-                    reviewStr.push(reviewLog.reviewer + ":" + (reviewLog.approved == true ? "Approved" : "Rejected"));
-                });
-                row[7] = reviewStr.join(", ");
+        for(var i = 0; i < gridReport.columns.length; i++){
+            var column = gridReport.columns[i];
+            if(column.type == 'reviewlog'){
+                column.type = 'text';
+                indexOfReviewLog = i;
+                break;
             }
+        }
+        if(indexOfReviewLog == undefined)return gridReport;
+
+        _.each(gridReport.data, function(row){
+            var reviewLogs = row[indexOfReviewLog];
+            if(reviewLogs == undefined)return;
+            row[indexOfReviewLog] = convertReviewLogToStr(reviewLogs);
         });
         return gridReport;
     }
@@ -40,19 +46,26 @@ var JobDashboardRouter = new Marionette.AppRouter({
     }
 });
 
+function convertReviewLogToStr(reviewLogs){
+    var reviewStr = [];
+    _.each(reviewLogs, function(reviewLog){
+        reviewStr.push(reviewLog.reviewer + ":" + (reviewLog.approved == true ? "Approved" : "Rejected"));
+    });
+    return reviewStr.join(", ");
+}
 Application.subscribe('/core/jobstatusupdate', function(message){
     /*  If the dashboard is not currently displayed then just return - when the user chooses to see the dashboard
         (at a later point) the latest data from the server will in any case be fetched - no need to update the data now
      */
     if(dashboardView == undefined || dashboardView.isDestroyed())return;
 
-    /* The dashboard is currently active - send the updated data to the GridView (which expects an array of changes) */
-    /* First convert the reviewlog object into a string */
-    var reviewStr = [];
-    _.each(message.data[0].updatedData[7], function(reviewLog){
-        reviewStr.push(reviewLog.reviewer + ":" + (reviewLog.approved == true ? "Approved" : "Rejected"));
+    /* First convert the reviewlog object into a string IF necessary */
+    if(indexOfReviewLog != undefined) {
+        var reviewLogs = message.data[0].updatedData[indexOfReviewLog];
+        if(reviewLogs != undefined)
+            message.data[0].updatedData[indexOfReviewLog] = convertReviewLogToStr(reviewLogs);
+    }
 
-    });
-    message.data[0].updatedData[7] = reviewStr.join(", ");
+    /* The dashboard is currently active - send the updated data to the GridView (which expects an array of changes) */
     dashboardView.updateData([message.data[0].updatedData]);
 });
