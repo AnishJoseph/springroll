611a6e0dc10e9e6f419991e8b9d2cbe5c9fa96d7
Anish Joseph <joseph.anish@gmail.com>
2017-03-20 16:53:35 +0530

    Fixed the ajax errors

--- src/springroll-web/src/main/webapp/assets/lib/framework/App.js
+++ src/springroll-web/src/main/webapp/assets/lib/framework/App.js
@@ -272,31 +272,31 @@ class Application {
                 406:function(message){   //NOT_ACCEPTABLE
                     // Check if this is already handled in the business logic
                     if(message.errorHandled == undefined) {
-                        that.Indicator.showErrorMessage({message:message.responseText});
+                        that.showErrorNotification(message.responseText);
                     }
                 },
                 409:function(message){   //CONFLICT BUSINESS VIOLATIONS
                     // Check if this is already handled in the business logic
                     if(message.errorHandled == undefined) {
                         _.each(message.responseJSON, function (violation) {
-                            that.Indicator.showErrorMessage({message:violation.field + ': ' + violation.message});
+                            that.showErrorNotification("Field : " + violation.field + ': ' + violation.message);
                         });
                     }
                 },
-                400:function(message){   //BAD_MESSAGE CONSTRAINT VIOLATIONS
+                400:function(message){   //BAD_MESSAGE / BAD_REQUEST CONSTRAINT VIOLATIONS
                     // Check if this is already handled in the business logic
                     if(message.errorHandled == undefined) {
                         _.each(message.responseJSON, function (violation) {
                             _.each(Object.keys(violation), function(field) {
                                 /* First localize the field name */
-                                var localizedFieldName = Localize(field);
+                                var localizedFieldName = that.Localize(field);
                                 if (violation[field].includes('{0}')){
                                     /* if the error message is a custom message which includes  the field name  i.e message string contains '{0}'
                                      then dont show the field name separately - just create the message as per the template
                                      */
-                                    that.Indicator.showErrorMessage({message: Localize(violation[field], [localizedFieldName])});
+                                    that.showErrorNotification(that.Localize(violation[field], [localizedFieldName]));
                                 } else {
-                                    that.Indicator.showErrorMessage({message: localizedFieldName +  " : " + Localize(violation[field])});
+                                    that.showErrorNotification(localizedFieldName +  " : " + that.Localize(violation[field]));
                                 }
                             });
                         });
