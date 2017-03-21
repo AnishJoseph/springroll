2653e814fdb5c0e2698b13dcee8649917baaf904
Anish Joseph <joseph.anish@gmail.com>
2017-03-21 12:18:35 +0530

    Cleaned up package.json

--- src/springroll-web/src/main/webapp/assets/lib/framework/Springrollcore.js
+++ src/springroll-web/src/main/webapp/assets/lib/framework/Springrollcore.js
@@ -1,6 +1,6 @@
 import 'react-select/dist/react-select.css';
 import 'react-date-picker/index.css'
 require("main.scss");
-require("bootstrap/dist/css/bootstrap.min.css");
+//require("bootstrap/dist/css/bootstrap.min.css");
 
 require('ReviewAlertWrapper');
