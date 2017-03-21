c22d748a3f9443afb93d65b47b1d9741d187b58b
Anish Joseph <joseph.anish@gmail.com>
2017-03-21 11:59:39 +0530

    Alert summay beautifcication

--- src/springroll-web/src/main/webapp/assets/lib/framework/AlertSummary.jsx
+++ src/springroll-web/src/main/webapp/assets/lib/framework/AlertSummary.jsx
@@ -6,21 +6,21 @@ import { AlertFilters } from 'SpringrollActionTypes';
 class AlertSummary extends React.Component {
     render() {
         return (
-            <div>
-                <div onClick={() => this.props.setAlertFilter(AlertFilters.ALERT_FILTER_INFO)} className="alertInfo navbar-right" data-toggle="tooltip" title={Application.Localize('ui.view.info')}>
-                    <span className="alertLabel  glyphicon glyphicon-info-sign" aria-hidden="true"></span>
-                    <span className="alertInfoCount alertCount">{this.props.infoAlertsLen}</span>
-                </div>
-
-                <div onClick={() => this.props.setAlertFilter(AlertFilters.ALERT_FILTER_ERROR)} className="alertError navbar-right" data-toggle="tooltip" title={Application.Localize('ui.view.errors')}>
-                    <span className="alertLabel  glyphicon glyphicon-warning-sign" aria-hidden="true"/>
-                    <span className="alertErrorCount alertCount">{this.props.errorAlertsLen}</span>
-                </div>
-
-                <div onClick={() => this.props.setAlertFilter(AlertFilters.ALERT_FILTER_ACTION)} className="alertAction navbar-right" data-toggle="tooltip" title={Application.Localize('ui.view.approvals')}>
+            <div className="alert-summary-container navbar-right">
+                <span onClick={() => this.props.setAlertFilter(AlertFilters.ALERT_FILTER_ACTION)} className="alertAction " data-toggle="tooltip" title={Application.Localize('ui.view.approvals')}>
                     <span  className="alertLabel   glyphicon glyphicon-ok-sign" aria-hidden="true"/>
                     <span className="alertActionCount alertCount">{this.props.actionAlertsLen}</span>
-                </div>
+                </span>
+
+                <span onClick={() => this.props.setAlertFilter(AlertFilters.ALERT_FILTER_ERROR)} className="alertError " data-toggle="tooltip" title={Application.Localize('ui.view.errors')}>
+                    <span className="alertLabel  glyphicon glyphicon-warning-sign" aria-hidden="true"/>
+                    <span className="alertErrorCount alertCount">{this.props.errorAlertsLen}</span>
+                </span>
+
+                <span  onClick={() => this.props.setAlertFilter(AlertFilters.ALERT_FILTER_INFO)} className="alertInfo " data-toggle="tooltip" title={Application.Localize('ui.view.info')}>
+                    <span className="alertLabel  glyphicon glyphicon-info-sign" aria-hidden="true"></span>
+                    <span className="alertInfoCount alertCount">{this.props.infoAlertsLen}</span>
+                </span>
             </div>
         );
     }
