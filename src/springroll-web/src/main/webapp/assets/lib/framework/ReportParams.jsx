e1ec261b5237b33a8e29c90a2260ee8236b03e5c
Anish Joseph <joseph.anish@gmail.com>
2017-03-28 06:13:55 +0530

    Minor formatting

--- src/springroll-web/src/main/webapp/assets/lib/framework/ReportParams.jsx
+++ src/springroll-web/src/main/webapp/assets/lib/framework/ReportParams.jsx
@@ -26,6 +26,7 @@ class ReportParams extends React.Component {
            <div className="panel panel-default">
                <div className="panel-body">
                    <form className="form-inline report-param-form">
+                       <div className="container-fluid">
                        {
                            this.props.params.map((parameter) => {
                                if(parameter.lovList != null){
@@ -37,14 +38,14 @@ class ReportParams extends React.Component {
                                 )
                                } else if ( parameter.javaType == "java.lang.Integer" || parameter.javaType == "java.lang.Long" || parameter.javaType == "java.math.BigInteger" || parameter.javaType == "java.lang.Short"){
                                    return (
-                                       <div key={parameter.name} className="form-group rep-param">
+                                       <div key={parameter.name} className="form-group rep-param col-md-3">
                                            <div>{Application.Localize(parameter.name)}</div>
                                            <input required className="form-control " type="number" step="1" onChange={ (value) => this.onChange(parameter.name, value.target.value)}   value={this.state.paramValues[parameter.name]}/>
                                        </div>
                                    )
                                } else if ( parameter.javaType == "java.lang.Double"  || parameter.javaType == "java.lang.Float" || parameter.javaType == "java.math.BigDecimal"){
                                    return (
-                                       <div key={parameter.name} className="form-group rep-param">
+                                       <div key={parameter.name} className="form-group rep-param col-md-3">
                                            <div>{Application.Localize(parameter.name)}</div>
                                            <input required className="form-control " type="number" onChange={ (value) => this.onChange(parameter.name, value.target.value)}  value={this.state.paramValues[parameter.name]}/>
                                        </div>
@@ -59,14 +60,14 @@ class ReportParams extends React.Component {
                                    )
                                } else if (parameter.javaType == "java.time.LocalDate" || (parameter.javaType == "java.time.LocalDateTime" && (parameter.setTime === 'START_OF_DAY' || parameter.setTime === 'END_OF_DAY'))){
                                    return (
-                                       <div key={parameter.name} className="form-group rep-param">
+                                       <div key={parameter.name} className="form-group rep-param col-md-3">
                                            <div>{Application.Localize(parameter.name)}</div>
                                            <DatePicker onChange={ (value) => this.onChange(parameter.name, value)}  value={this.state.paramValues[parameter.name]}/>
                                        </div>
                                    )
                                } else if (parameter.javaType == "java.time.LocalDateTime"){
                                    return (
-                                       <div key={parameter.name} className="form-group rep-param">
+                                       <div key={parameter.name} className="form-group rep-param col-md-3">
                                            <div>{Application.Localize(parameter.name)}</div>
                                            //<DupDateTimePicker onChange={ (value) => this.onChange(parameter.name, value)}  value={this.state.paramValues[parameter.name]}/>
                                            <div>DATETIME TBD</div>
@@ -74,7 +75,7 @@ class ReportParams extends React.Component {
                                    )
                                } else {
                                    return (
-                                       <div key={parameter.name} className="form-group rep-param col-md-2">
+                                       <div key={parameter.name} className="form-group rep-param col-md-3">
                                            <div>{Application.Localize(parameter.name)}</div>
                                            <input required className="form-control " value={this.state.paramValues[parameter.name]} type="text" onChange={ (value) => this.onTextInput(parameter.name, value.target.value)}  value={this.state.paramValues[parameter.name]}/>
                                        </div>
@@ -82,7 +83,7 @@ class ReportParams extends React.Component {
                                }
                            })
                        }
-
+                       </div>
                        <div  className="formSubmit">
                            <span title={Application.Localize('Apply')} onClick={() => this.props.onParamsSelected(this.state.paramValues)} className="form-control submit glyphicon glyphicon-ok"></span>
                        </div>
