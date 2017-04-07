62448020ddd7cbd2724735aa4b327576d7d72f07
Anish Joseph <joseph.anish@gmail.com>
2017-04-07 15:12:42 +0530

    Cleanup

--- src/springrollexample/springrollexample-web/src/main/webapp/assets/lib/modules/SubMenuSimple.jsx
+++ src/springrollexample/springrollexample-web/src/main/webapp/assets/lib/modules/SubMenuSimple.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import Application from 'App';
-import Module3_2 from 'Module3.2.jsx';
 
-const SubMenuSimple = ({ children }) => {
+const SubMenuSimpleOne = ({ children }) => {
     return (
-        <div>Third First</div>
+        <div>Simple Submenu One</div>
+    );
+};
+const SubMenuSimpleTwo = ({ children }) => {
+    return (
+        <div>Simple Submenu Two</div>
     );
 };
 
-/* This is an example of a  menu with submenus
+/* This is an example of submenus with each submenu points to a different component
  - its a top level menu (will be shown second, as the parent index is set to 2
  - it has 2 submenus - order in which they appear depends on the value of index
  - a form is rendered when clicked - what gets rendered is specified by 'component'
@@ -21,8 +25,8 @@ Application.addMenu({
     index: 3,
     type: "menuitem",
     route: 'SubMenuSimple/SM1',
-    component : Module3_1,
-    parentTitle: 'module3_name',
+    component : SubMenuSimpleOne,
+    parentTitle: 'ui.submenu.simple',
     parentIndex : 2
 });
 Application.addMenu({
@@ -30,14 +34,7 @@ Application.addMenu({
     index: 3,
     type: "menuitem",
     route: 'SubMenuSimple/SM2',
-    component : Module3_2,
-    parentTitle: 'module3_name',
+    component : SubMenuSimpleTwo,
+    parentTitle: 'ui.submenu.simple',
     parentIndex : 2
 });
-
-module.exports = Module3_1;
-
-
-
-
-
--- src/springrollexample/springrollexample-web/src/main/webapp/assets/lib/modules/SubmenuCommon.jsx
+++ src/springrollexample/springrollexample-web/src/main/webapp/assets/lib/modules/SubmenuCommon.jsx
@@ -14,8 +14,6 @@ const SubmenuCommon = (props) => {
     );
 };
 
-module.exports = Module3_3;
-
 /* This is an example of submenus with each submenu pointing to the <b> SAME</b> component
  - its a top level menu (will be shown third, as the parent index is set to 3
  - it has 2 submenus - order in which they appear depends on the value of index
@@ -24,28 +22,32 @@ module.exports = Module3_3;
    determine which menu was clicked from the props.params.submodule
  - the name of the parent menu is defined by parentTitle
  - title and parentTitle is localized (i.e the values are looked up in the properties file
+
+ The route definition will be the same for all menu items, while the routeOnClick value depends on the menu item
+ This kind of menu can be used when the number of submenus is known a priori - i.e we know at dev time the number
+ of submenus needed. For dynamic submenus look at MDM
  */
 
 Application.addMenu({
-    title: 'ui.menu.third.third',
+    title: 'ui.menu.submenuCommon.one',
     index: 3,
     type: "menuitem",
-    route: 'M4/:submodule',
-    component : Module3_3,
-    parentTitle: 'module4_name',
+    route: 'SMC/:submodule',
+    component : SubmenuCommon,
+    parentTitle: 'ui.menu.submenuCommon',
     parentIndex : 3,
-    routeOnClick: 'M4/one'
+    routeOnClick: 'SMC/one'
 });
 
 Application.addMenu({
-    title: 'ui.menu.third.fourth',
+    title: 'ui.menu.submenuCommon.two',
     index: 3,
     type: "menuitem",
-    route: 'M4/:submodule',
-    component : Module3_3,
-    parentTitle: 'module4_name',
+    route: 'SMC/:submodule',
+    component : SubmenuCommon,
+    parentTitle: 'ui.menu.submenuCommon',
     parentIndex : 3,
-    routeOnClick: 'M4/two'
+    routeOnClick: 'SMC/two'
 });
 
 
