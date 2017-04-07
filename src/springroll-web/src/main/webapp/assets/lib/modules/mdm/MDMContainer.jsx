e629b60226378fa09a24c058d9639abf0e631bea
Anish Joseph <joseph.anish@gmail.com>
2017-04-07 15:38:28 +0530

    Comments

--- src/springroll-web/src/main/webapp/assets/lib/modules/mdm/MDMContainer.jsx
+++ src/springroll-web/src/main/webapp/assets/lib/modules/mdm/MDMContainer.jsx
@@ -20,6 +20,12 @@ const mapDispatchToProps = (dispatch) => bindActionCreators({
 const MDMContainer = connect(mapStateToProps, mapDispatchToProps)(MDM);
 export default MDMContainer;
 
+/*  This is to handle the case in MDM where the number of MDM masters are unknown at compile time.
+    Couple of points here - regardless of the MDM master chosen the component is always MDMContainer.
+    Here we define the routes but the actual push of the route happens in MDM.jsx when a master is chosen. The route
+    itself is changed using router.push - this.props.router.push('/mdm/' + masterName);
+ */
+
 const routes = (
     <Route key="mdm" path="/mdm" component={MDMContainer}>
         <Route key="mdmMaster" path="/mdm/:master" component={MDMContainer}/>
