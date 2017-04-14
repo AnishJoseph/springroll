1547cd936ce1a264a0ec6d0d17af9fbdd6f57371
Anish Joseph <joseph.anish@gmail.com>
2017-04-14 15:47:19 +0530

    Cleaned up react-select scss

--- src/springroll-web/src/main/webapp/assets/lib/framework/Springrollcore.js
+++ src/springroll-web/src/main/webapp/assets/lib/framework/Springrollcore.js
@@ -1,7 +1,5 @@
-import 'react-select/dist/react-select.css';
-import 'react-date-picker/index.css'
+import 'react-date-picker/index.css';
 require("main.scss");
-//require("bootstrap/dist/css/bootstrap.min.css");
 require("react-bootstrap-table/dist/react-bootstrap-table-all.min.css");
 require("react-datetime/css/react-datetime.css");
 
